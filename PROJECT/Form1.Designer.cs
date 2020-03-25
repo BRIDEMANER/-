494d4152a53f5f7b90bc92e8e79dcab88a970349
BRIDEMANER <51445847+BRIDEMANER@users.noreply.github.com>
2020-03-26 02:13:22 +0300

    Add files via upload

---
+++ PROJECT/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cube = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labeScr = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cube)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cube
+            // 
+            this.cube.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.cube.Location = new System.Drawing.Point(0, 0);
+            this.cube.Name = "cube";
+            this.cube.Size = new System.Drawing.Size(40, 40);
+            this.cube.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cube.TabIndex = 0;
+            this.cube.TabStop = false;
+            this.cube.Visible = false;
+            this.cube.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // labeScr
+            // 
+            this.labeScr.AutoSize = true;
+            this.labeScr.Location = new System.Drawing.Point(481, 9);
+            this.labeScr.Name = "labeScr";
+            this.labeScr.Size = new System.Drawing.Size(35, 13);
+            this.labeScr.TabIndex = 1;
+            this.labeScr.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 417);
+            this.Controls.Add(this.labeScr);
+            this.Controls.Add(this.cube);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cube)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cube;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labeScr;
+    }
+}
+
---
+++ PROJECT/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox[] snake = new PictureBox[400];
+        public int rI, rj;
+        public PictureBox fruit;
+        public int dirX, dirY;
+        public int hieght = 875;
+        public int width = 900;
+        public int size = 40;
+        public int score=0;
+        public int i;        
+        public  Label labelscore;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown+= new KeyEventHandler(Move);
+            this.Width = width;
+            this.Height = hieght;
+            generatemap();
+            timer1.Tick += new EventHandler(_update);
+            timer1.Interval = 500;
+            timer1.Start();
+            dirX = 1;
+            dirY = 0;
+            Label labelScore = new Label();
+            labeScr.Text = "Score: 0";
+            labeScr.Location =new Point(810,10);  
+            this.Controls.Add(labelscore);
+            snake[0] = new PictureBox();
+            snake[0].Location = new Point(201, 201);
+            snake[0].Size = new Size(size-1, size-1);
+            snake[0].BackColor = Color.Green;                          
+            this.Controls.Add(snake[0]);
+            fruit = new PictureBox();
+            fruit.BackColor = Color.Yellow;
+            fruit.Size = new Size(size, size);
+            _generateFruit();
+            this.Text = "Змейка";   
+           
+        }
+        private void _generateFruit()
+        {
+            Random rnd = new Random();
+            rI = rnd.Next(0,hieght-size);
+            int tempI = rI % size;
+            rI -= tempI;
+            //
+            rj = rnd.Next(0, width - size);
+            int tempj = rj % size;
+            rj -= tempj;
+            fruit.Location = new Point(rI, rj);
+            this.Controls.Add(fruit);
+            rj++;
+            rI++;
+        }
+        private void _checkBorders()
+        {
+            if (snake[0].Location.X < 0)
+            {
+                for (int _i = 1; _i <= score; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                score = 0;
+                labeScr.Text = "Score: " + score;
+                dirX = 1;
+            }
+            if (snake[0].Location.X > hieght)
+            {
+                for (int _i = 1; _i <= score; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                score = 0;
+                labeScr.Text = "Score: " + score;
+                dirX = -1;
+            }
+            if (snake[0].Location.Y < 0)
+            {
+                for (int _i = 1; _i <= score; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                score = 0;
+                labeScr.Text = "Score: " + score;
+                dirY = 1;
+            }
+            if (snake[0].Location.Y > hieght)
+            {
+                for (int _i = 1; _i <= score; _i++)
+                {
+                    this.Controls.Remove(snake[_i]);
+                }
+                score = 0;
+                labeScr.Text = "Score: " + score;
+                dirY = -1;
+            }
+        }
+        private void _moveSnake()
+        {
+            for (int i = score; i >= 1; i--)
+            {
+                snake[i].Location = snake[i - 1].Location;
+               
+            }
+             snake[0].Location = new Point(snake[0].Location.X + dirX * (size), snake[0].Location.Y + dirY * (size));
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+         void Move(object sender, KeyEventArgs e)
+        {   switch (e.KeyData.ToString())
+            {
+                case "Up":
+                   dirY =-1;
+                    dirX = 0;
+                    break;
+                case "Down":
+                    dirY = 1;
+                    dirX = 0;
+                    break;
+                case "Right":
+                    dirX = 1;
+                    dirY = 0;
+                    break;
+                case "Left":
+                    dirX = -1;
+                    dirY = 0;
+                    break;
+            }           
+        }
+        private void _eat()
+        {
+            if(snake[0].Location.X==rI && snake[0].Location.Y==rj)
+            {
+                labeScr.Text = "score" + ++score;
+                snake[score] = new PictureBox();
+                snake[score].Location = new Point(snake[score - 1].Location.X + 40 * dirX, snake[score - 1].Location.Y - 40 * dirY);
+                snake[score].Size = new Size(size-1, size-1);
+                snake[score].BackColor = Color.GreenYellow;
+                this.Controls.Add(snake[score]);
+                _generateFruit();
+               
+                
+            }
+        }
+
+        private void _update(Object myobject,EventArgs e)
+        {
+            _eat();
+            _moveSnake();
+            _eatSelf();
+            _checkBorders();
+        }
+        private void _eatSelf()
+        {
+            for (int _i = 1; _i < score; _i++)
+            {
+                if (snake[0].Location == snake[_i].Location)
+                {
+                    for (int _j = _i; _j <= score; _j++)
+                        this.Controls.Remove(snake[_j]);
+                    score = score - (score - _i + 1);
+                    labeScr.Text = "Score: " + score;
+
+                }
+            }
+        }
+
+      private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void generatemap()
+        {
+            for(int i= 0; i< width/size; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.Black;
+                pic.Location = new Point(0,size*i);
+                pic.Size = new Size(width , 1);
+                this.Controls.Add(pic);
+            }
+            for(int i=0;i<=hieght/size;i++) 
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.Black;
+                pic.Location = new Point( size * i,0);
+                pic.Size = new Size(1,width);
+                this.Controls.Add(pic);
+
+            }
+        }
+            
+               
+       
+    }
+}
